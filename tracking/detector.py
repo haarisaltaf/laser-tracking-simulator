dc61be0781d113e8c3695317f4c6e36bc457fe8c
Haaris Altaf <altafhaaris@gmail.com>
2025-06-17 18:13:12 +0100

    added thresholding to detector.py

--- tracking/detector.py
+++ tracking/detector.py
@@ -1,1 +1,25 @@
-# detector.py placeholder
+from cv2 import imread, imshow, waitKey, destroyAllWindows, threshold, THRESH_BINARY
+
+# constants
+global PATH_TO_FRAMES
+PATH_TO_FRAMES = "../simulator/frames"
+
+# TODO: grab the number of frames in the directory; currently assuming 30 frames
+global DEFAULT_NUMBER_OF_FRAMES
+DEFAULT_NUMBER_OF_FRAMES = 30
+
+# grab frame in grayscale mode
+currentFrame = imread(PATH_TO_FRAMES+"/frame0.png", 0)
+imshow("frame0", currentFrame)
+# waits till user presses any key to stop python kernel crashing
+waitKey(0)
+# destroys open windows after key press
+destroyAllWindows()
+
+# making only the pixels above 200 brightness visible, the rest are black.
+thresholdValue, thresholdFrame = threshold(currentFrame, 200, 255, THRESH_BINARY)
+imshow("threshFrame0", thresholdFrame)
+# waits till user presses any key to stop python kernel crashing
+waitKey(0)
+# destroys open windows after key press
+destroyAllWindows()
