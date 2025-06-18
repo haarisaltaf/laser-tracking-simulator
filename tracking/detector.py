56e02b09db0b3cf8998de2b8275467426d509b7f
Haaris Altaf <altafhaaris@gmail.com>
2025-06-18 22:34:18 +0100

    detector.py functioning and draws red border around the blob detected.

--- tracking/detector.py
+++ tracking/detector.py
@@ -10,36 +10,48 @@ PATH_TO_FRAMES = "../simulator/frames"
 global DEFAULT_NUMBER_OF_FRAMES
 DEFAULT_NUMBER_OF_FRAMES = 30
 
-# grab frame in grayscale mode
-currentFrame = imread(PATH_TO_FRAMES+"/frame0.png", 0)
-imshow("frame0", currentFrame)
-# waits till user presses any key to stop python kernel crashing
-waitKey(0)
-# destroys open windows after key press
-destroyAllWindows()
 
-# Setting the parameters for the simple blob detector
-params = SimpleBlobDetector_Params()
-params.minThreshold = 190
-params.maxThreshold = 255
+def grabBlobKeypoint():
+    # grab frame in grayscale mode
+    currentFrame = imread(PATH_TO_FRAMES+"/frame0.png", 0)
+    imshow("frame0", currentFrame)
+    # waits till user presses any key to stop python kernel crashing
+    waitKey(0)
+    # destroys open windows after key press
+    destroyAllWindows()
 
-params.filterByCircularity = True
+    # # making only the pixels above 200 brightness visible, the rest are black.
+    # thresholdValue, thresholdFrame = threshold(currentFrame, 200, 255, THRESH_BINARY)
 
-params.blobColor = 255
-# params.minArea = 5
+    # Setting the parameters for the simple blob detector
+    params = SimpleBlobDetector_Params()
+    params.minThreshold = 190
+    params.maxThreshold = 255
 
-params.filterByCircularity = True
-params.minCircularity = 0.4
+    params.filterByCircularity = True
 
-# Creating the blob detector using the parameters set
-detector = SimpleBlobDetector_create(params)
-blobsDetected = detector.detect(currentFrame)
-print(blobsDetected)
+    params.blobColor = 255
+    # params.minArea = 5
 
-output = drawKeypoints(currentFrame, blobsDetected, array([]), (0, 0, 255),
-                           DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
- 
-# Show the output
-imshow("Blobs Detected", output)
-waitKey(0)
-destroyAllWindows()
+    params.filterByCircularity = True
+    params.minCircularity = 0.5
+
+    # Creating the blob detector using the parameters set
+    detector = SimpleBlobDetector_create(params)
+    blobsDetected = detector.detect(currentFrame)
+    print(blobsDetected)
+
+    output = drawKeypoints(currentFrame, blobsDetected, array([]), (0, 0, 255),
+                               DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+     
+    # Show the output
+    imshow("Blobs Detected", output)
+    waitKey(0)
+    destroyAllWindows()
+
+
+grabBlobKeypoint()
+
+# simulating the fast steering mirror
+def fastSteeringMirror():
+    pass
