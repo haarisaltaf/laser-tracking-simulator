2d8f179bd8b6089409494caf6c0608f5cc6b6d12
Haaris Altaf <altafhaaris@gmail.com>
2025-06-18 18:22:18 +0100

    detector.py functioning and draws red border around the blob detected.

--- tracking/detector.py
+++ tracking/detector.py
@@ -1,4 +1,6 @@
-from cv2 import imread, imshow, waitKey, destroyAllWindows, threshold, THRESH_BINARY, SimpleBlobDetector
+from cv2 import imread, imshow, waitKey, destroyAllWindows, threshold, THRESH_BINARY, SimpleBlobDetector_create, SimpleBlobDetector_Params, DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS, drawKeypoints
+from numpy import array
+
 
 # constants
 global PATH_TO_FRAMES
@@ -16,14 +18,37 @@ waitKey(0)
 # destroys open windows after key press
 destroyAllWindows()
 
-# making only the pixels above 200 brightness visible, the rest are black.
+# # making only the pixels above 200 brightness visible, the rest are black.
 thresholdValue, thresholdFrame = threshold(currentFrame, 200, 255, THRESH_BINARY)
-imshow("threshFrame0", thresholdFrame)
-# waits till user presses any key to stop python kernel crashing
-waitKey(0)
-# destroys open windows after key press
-destroyAllWindows()
+# imshow("threshFrame0", thresholdFrame)
+# # waits till user presses any key to stop python kernel crashing
+# waitKey(0)
+# # destroys open windows after key press
+# destroyAllWindows()
 
-detector = SimpleBlobDetector()
+# Setting the parameters for the simple blob detector
+params = SimpleBlobDetector_Params()
+params.minThreshold = 200
+params.maxThreshold = 255
+
+params.filterByCircularity = True
+
+params.blobColor = 255
+# params.minArea = 5
+
+params.filterByCircularity = True
+params.minCircularity = 0.4
+
+
+
+detector = SimpleBlobDetector_create(params)
 blobsDetected = detector.detect(thresholdFrame)
 print(blobsDetected)
+
+output = drawKeypoints(thresholdFrame, blobsDetected, array([]), (0, 0, 255),
+                           DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+ 
+# Show the output
+imshow("Blobs Detected", output)
+waitKey(0)
+destroyAllWindows()
